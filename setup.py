4f5f57d2daedfa22b2682989eb4f520d73a1bf93
Ziming Ding <uowzd01@hotmail.com>
2020-07-14 11:53:50 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Ziming Ding',
+    author_email='uowzd01@hotmail.com',
+    description='A utility for backing up PostgreSQL databases.',
+    long_description=long_description,
+    long_description_content='text/markdown',
+    url='https://github.com/zimingding/pgbackup',
+    packages=find_packages('src')
+)
