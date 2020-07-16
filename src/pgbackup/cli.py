92ad5e00c8c1586a053acf8201851d1bff9e2824
Ziming Ding <uowzd01@hotmail.com>
2020-07-16 17:03:15 +1000

    Wiring the pieces together

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -12,10 +12,28 @@ def create_parser():
     """)
 
     parser.add_argument("url", help="URL of the database to backup")
-    parser.add_argument("--driver",
+    parser.add_argument("--driver", "-d",
             help="how & where to store backup",
             nargs=2,
+            metavar=("DRIVER", "DESTINATION"),
             action=DriverAction,
             required=True)
 
     return parser
+
+def main():
+    import time
+    import boto3
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        storage.local(dump.stdout, outfile)
--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -7,3 +7,10 @@ def dump(url):
     except OSError as err:
         print(f"Error: {err}")
         sys.exit(1)
+
+def dump_file_name(url, timestamp=None):
+    db_name = url.split("/")[-1]
+    if timestamp:
+        return f"{db_name}-{timestamp}.sql"
+    else:
+        return f"{db_name}.sql"
